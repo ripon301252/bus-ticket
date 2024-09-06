748ee14a3f4a96432d9586681f79da81f8e3f6cd
ripon301252 <93132951+ripon301252@users.noreply.github.com>
2024-09-07 04:37:47 +0600

    Create ticket.js

---
+++ ticket.js
@@ -0,0 +1,16 @@
+function busTicketElementById(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.add('bg-[#1ecc00]');
+}
+
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId,value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
