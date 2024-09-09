c1a4e2e76184557664ca09d073dad9121ea88841
ripon301252 <93132951+ripon301252@users.noreply.github.com>
2024-09-09 22:52:50 +0600

    Update ticket.js

--- ticket.js
+++ ticket.js
@@ -15,3 +15,45 @@ function setTextElementValueById(elementId,value){
     element.innerText = value;
 }
 
+
+
+
+document.getElementById('copupon-apply').addEventListener('keyup', function(event){
+    const text = event.target.value;
+     const deleteButton = document.getElementById('btn-apply');
+          deleteButton.style.backgroundColor = 'red'
+    if(text === 'NEW15'){
+        deleteButton.removeAttribute('disabled');
+        deleteButton.style.backgroundColor = 'green';
+        deleteButton.style.color = 'blue';
+    }
+    else{
+        deleteButton.setAttribute('disabled', true);
+    }
+
+    if(text === 'Couple 20'){
+        deleteButton.removeAttribute('disabled');
+        deleteButton.style.backgroundColor = 'green';
+        deleteButton.style.color = 'blue';
+    }
+    else{
+        deleteButton.setAttribute('disabled', true);
+    }
+})
+
+
+document.getElementById('btn-apply').addEventListener('click', function(){
+    const newAs = document.getElementById('new-15');
+    newAs.style.display = 'none'
+})
+
+document.getElementById('btn-apply').addEventListener('click', function(){
+    const couple = document.getElementById('couple-20');
+    couple.style.display = 'none'
+})
+
+
+
+
+
+  
