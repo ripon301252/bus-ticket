61a6a0d3bf28381aff9b4c805df2659a230245b5
ripon301252 <93132951+ripon301252@users.noreply.github.com>
2024-09-09 06:56:27 +0600

    Update bus-ticket.js

--- bus-ticket.js
+++ bus-ticket.js
@@ -4,11 +4,11 @@ function busTicketA1(){
     const selectedSeat = 1;
     const noSeat = 0;
     for(let i = 0; i <= 2; i++){
-            
-            if(i >= 2){
                 const seatLeft = getTextElementValueById('seat-left')
                 const leftSeat = seatLeft ;
                 setTextElementValueById('seat-left', leftSeat);
+            if(i >= 2){
+                
                 break;
             }
         }
